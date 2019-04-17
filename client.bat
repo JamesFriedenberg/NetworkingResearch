1129a3151bdde7d01bab8665a41cab1a90144e9b
Benjamin M <bcm023@gmail.com>
2019-04-17 11:17:39 -0400

    example files

---
+++ client.bat
@@ -0,0 +1,2 @@
+TIMEOUT /T 6
+"C:\Program Files\Epic Games\UE_4.20\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\tank.uproject" 192.168.0.3 -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -log -nosteam
---
+++ server.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Epic Games\UE_4.20\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\tank.uproject" TwinStickExampleMap -server -log -nosteam
